1911987b65dfa6583c34f2559800572297863ad1
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-08-16 16:39:30 -0400

    UserPost Entity added

--- French.Data/Entities/UserPost.cs
+++ French.Data/Entities/UserPost.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace French.Data.Entities;
 
 public class UserPost
 {
+    [Key]
+    public int UserPostId { get; set; }
+
+    [ForeignKey("Recipe")]
+    public int RecipeId { get; set; }
+
+    [MaxLength(100)]
+    public string ReviewText { get; set; } = string.Empty;
+
+    [Required]
+    public int ReviewRating { get; set; }
+
+    [Required]
+    public DateTime ReviewDate { get; set; }
 }
