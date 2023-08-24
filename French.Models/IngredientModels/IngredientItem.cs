da536e4b550067d3bf67f456de23c2748ce0e61e
Nathan Matlack <nathanielmatlack@gmail.com>
2023-08-24 10:57:07 -0400

    Updated Ingredient Model to show IngredientId

--- French.Models/IngredientModels/IngredientItem.cs
+++ French.Models/IngredientModels/IngredientItem.cs
@@ -3,6 +3,8 @@
 namespace French.Models.IngredientModels;
 
 public class IngredientItem {
+    [Required]
+    public int Id { get; set; }
     [Required, MaxLength(50)]
     public string Name { get; set; } = null!;
     [Required, MaxLength(1000)]
